d2a77c1874f3aac6b55203ce0017e6dcf38e20e7
codecaiine <akabrouyannickn@gmail.com>
2023-02-05 19:48:35 +0000

    Update user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,23 +6,23 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+
 use Tymon\JWTAuth\Contracts\JWTSubject;
+
 
 class User extends Authenticatable implements JWTSubject
 {
     use HasFactory, Notifiable;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
     protected $fillable = [
-        'username',
-        'slug',
-        'email',
-        'password',
-        'status',
+        'slug', 'username', 'email', 'password', 'address', 'status', 'picture', 'is_admin', 'verify', 'token',
     ];
+
     /**
      * The attributes that should be hidden for arrays.
      *
@@ -31,7 +31,7 @@ class User extends Authenticatable implements JWTSubject
     protected $hidden = [
         'password',
     ];
-  
+
     /**
      * Get the identifier that will be stored in the subject claim of the JWT.
      *
@@ -40,6 +40,7 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTIdentifier() {
         return $this->getKey();
     }
+
     /**
      * Return a key value array, containing any custom claims to be added to the JWT.
      *
@@ -47,5 +48,5 @@ class User extends Authenticatable implements JWTSubject
      */
     public function getJWTCustomClaims() {
         return [];
-    }    
+    }
 }
