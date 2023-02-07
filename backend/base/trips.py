41296aefa7d6dc85a6e120a8af8f82451eaf5772
codecaiine <akabrouyannickn@gmail.com>
2023-02-07 22:39:46 +0000

    Add django rest framework

---
+++ backend/base/trips.py
@@ -0,0 +1,62 @@
+trips = [
+    {
+        '_id': '1',
+        'name': 'Airpods Wireless Bluetooth Headphones',
+        'reason':
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        'departure_date': 12/12/2022,
+        'arrival_date': '12/12/2022',
+        'note': 'oaoaoaoaoaoaoaoaoao',
+        'user_name': '2',
+    },
+    {
+        '_id': '2',
+        'name': 'Airpods Wireless Bluetooth Headphones',
+        'reason':
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        'departure_date': 12/12/2022,
+        'arrival_date': '12/12/2022',
+        'note': 'oaoaoaoaoaoaoaoaoao',
+        'user_name': '2',
+    },
+    {
+        '_id': '3',
+        'name': 'Airpods Wireless Bluetooth Headphones',
+        'reason':
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        'departure_date': 12/12/2022,
+        'arrival_date': '12/12/2022',
+        'note': 'oaoaoaoaoaoaoaoaoao',
+        'user_name': '2',
+    },
+    {
+        '_id': '4',
+        'name': 'Airpods Wireless Bluetooth Headphones',
+        'reason':
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        'departure_date': 12/12/2022,
+        'arrival_date': '12/12/2022',
+        'note': 'oaoaoaoaoaoaoaoaoao',
+        'user_name': '2',
+    },
+    {
+        '_id': '5',
+        'name': 'Airpods Wireless Bluetooth Headphones',
+        'reason':
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        'departure_date': 12/12/2022,
+        'arrival_date': '12/12/2022',
+        'note': 'oaoaoaoaoaoaoaoaoao',
+        'user_name': '2',
+    },
+        {
+        '_id': '6',
+        'name': 'Airpods Wireless Bluetooth Headphones',
+        'reason':
+        'Bluetooth technology lets you connect it with compatible devices wirelessly High-quality AAC audio offers immersive listening experience Built-in microphone allows you to take calls while working',
+        'departure_date': 12/12/2022,
+        'arrival_date': '12/12/2022',
+        'note': 'oaoaoaoaoaoaoaoaoao',
+        'user_name': '2',
+    },
+]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from . trips import trips
 
 # Create your views here.
+
+def getRoutes(request):
+    routes = [
+    ]
+    return JsonResponse(routes, safe=False)
+
+def getTrips(request):
+    return JsonResponse(trips, safe=False)    
