decf2f1c8e08fa24afa14974d87757a76700a116
codecaiine <akabrouyannickn@gmail.com>
2023-02-08 15:38:49 +0000

    Setup jwt

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+)
+
 
 urlpatterns = [
+    path('users/login', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('trips/', views.getTrips, name="trips"),
     path('trips/<str:pk>/', views.getTrip, name="trip"),
