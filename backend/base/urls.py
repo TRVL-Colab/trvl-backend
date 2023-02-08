1c370cc64bc8a390da2299a93f8e565fc426bdb9
codecaiine <akabrouyannickn@gmail.com>
2023-02-08 00:19:57 +0000

    Setup backend

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.getRoutes, name="routes"),
     path('trips/', views.getTrips, name="trips"),
+    path('trips/<str:pk>/', views.getTrip, name="trip"),
 ]
--- backend/base/views.py
+++ backend/base/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from . trips import trips
 
 # Create your views here.
-
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
 
+@api_view(['GET'])
 def getTrips(request):
-    return JsonResponse(trips, safe=False)    
+    return Response(trips)    
+
+@api_view(['GET'])
+def getTrip(request, pk):
+    trip = None
+    for i in trips:
+        if i['_id'] == pk:
+            trip = i
+            break
+        
+    return Response(trip)    
