dd4e2e62d1a2c33082a57c045cd3a08da12c2568
codecaiine <akabrouyannickn@gmail.com>
2023-02-09 19:05:28 +0000

    Fix errors

--- backend/base/views/user_views.py
+++ backend/base/views/user_views.py
@@ -31,19 +31,17 @@ class MyTokenObtainPairView(TokenObtainPairView):
 @api_view(['POST'])
 def registerUser(request):
     data = request.data
-    try:
-        user = User.objects.create(
+ 
+    user = User.objects.create(
             first_name=data['name'],
-            username=data['email'],
-            email=data['email'],
+            username=data.get('email'),
+            email=data.get('email'),
+           
             password=make_password(data['password'])
         )
-
-        serializer = UserSerializerWithToken(user, many=False)
-        return Response(serializer.data)
-    except:
-        message = {'detail': 'User with this email already exists'}
-        return Response(message, status=status.HTTP_400_BAD_REQUEST)
+ 
+    serializer = UserSerializerWithToken(user, many=False)
+    return Response(serializer.data)
 
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
