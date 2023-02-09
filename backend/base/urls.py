e56179fd3242d9e575c2375fe9ab62d0eddf02b2
codecaiine <akabrouyannickn@gmail.com>
2023-02-09 13:00:37 +0000

    Add login with email

--- backend/base/urls.py
+++ backend/base/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 urlpatterns = [
     path('users/login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
-    
-    path('', views.getRoutes, name="routes"),
+
 
     path('users/register/', views.registerUser, name='register'),
 
